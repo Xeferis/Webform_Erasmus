3fc37fa591fbdd05b9759a20bc9c4ebe5f9c1547
Xeferis <florian@hillebold.de>
2023-05-12 09:47:09 +0200

    added start files

---
+++ main_server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for
+
+server = Flask(__name__, template_folder="templates")
+
+
+@server.route('/')
+def start():
+    return render_template('index.html')
+
+
+@server.route('/userform', method=['GET', 'POST'])
+def input_form():
+    return render_template('input_mask.html')
+
+
+server.run("0.0.0.0", "5005")
