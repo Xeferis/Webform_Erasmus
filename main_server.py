71eaacee431ed4706dd181b2b87452a4c821f653
Xeferis <florian@hillebold.de>
2023-05-23 17:36:46 +0200

    Changes ?!

--- main_server.py
+++ main_server.py
@@ -1,7 +1,11 @@
+import sqlite3
 from flask import Flask, render_template, redirect, url_for
 
+
+# Init
 server = Flask(__name__, template_folder="templates")
 
+db = sqlite3.connect()
 
 @server.route('/')
 def start():
